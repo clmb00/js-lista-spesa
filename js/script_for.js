a40b19695747c3eabd59d0739f9ec73b16d813b5
clmb00 <paolo.colombo2000@gmail.com>
2022-10-18 15:25:54 +0200

    [ADD] for

--- js/script_for.js
+++ js/script_for.js
@@ -1,2 +1,10 @@
 const alimenti = ["Uova", "Patate", "Pomodori", "Pasta", "Cioccolato", "Panna", "Burro", "Olio", "Mele", "Pere", "Mandarini"];
 
+const ElementUl = document.createElement('ul');
+document.getElementsByTagName('body')[0].append(ElementUl);
+
+for ( let i = 0; i < alimenti.length; i++){
+  const ElementLi = document.createElement('li');
+  ElementLi.innerHTML = alimenti[i];
+  ElementUl.append(ElementLi);
+}
