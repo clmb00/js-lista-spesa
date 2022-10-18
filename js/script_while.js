3a5dc3f7099b188089a4a05c122234132033d486
clmb00 <paolo.colombo2000@gmail.com>
2022-10-18 15:27:18 +0200

    [ADD] while

--- js/script_while.js
+++ js/script_while.js
@@ -1,1 +1,12 @@
 const alimenti = ["Uova", "Patate", "Pomodori", "Pasta", "Cioccolato", "Panna", "Burro", "Olio", "Mele", "Pere", "Mandarini"];
+let c = 0;
+
+const ElementUl = document.createElement('ul');
+document.getElementsByTagName('body')[0].append(ElementUl);
+
+while (c < alimenti.length){
+  const ElementLi = document.createElement('li');
+  ElementLi.innerHTML = alimenti[c];
+  ElementUl.append(ElementLi);
+  c++;
+}
