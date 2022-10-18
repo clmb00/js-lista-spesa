765d17fa6760dfccd5aa93b7294bde829e76dc85
clmb00 <paolo.colombo2000@gmail.com>
2022-10-18 15:33:15 +0200

    [ADD] while solution 2

--- js/script_while.js
+++ js/script_while.js
@@ -1,10 +1,22 @@
 const alimenti = ["Uova", "Patate", "Pomodori", "Pasta", "Cioccolato", "Panna", "Burro", "Olio", "Mele", "Pere", "Mandarini"];
-let c = 0;
 
 const ElementUl = document.createElement('ul');
 document.getElementsByTagName('body')[0].append(ElementUl);
 
+let c = 0;
+
+// SOLUZIONE 1 WHILE
+/*
 while (c < alimenti.length){
+  const ElementLi = document.createElement('li');
+  ElementLi.innerHTML = alimenti[c];
+  ElementUl.append(ElementLi);
+  c++;
+}
+*/
+
+// SOLUZIONE 2 WHILE
+while (ElementUl.childElementCount != alimenti.length){
   const ElementLi = document.createElement('li');
   ElementLi.innerHTML = alimenti[c];
   ElementUl.append(ElementLi);
